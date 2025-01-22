11210a866e1ecab72116fe7a46af8bddead8ec74
Aditya Pandey <adityapandey0502@gmail.com>
2025-01-22 15:15:47 +0530

    Second change commit

--- practice1.py
+++ practice1.py
@@ -1,1 +1,3 @@
 print('Hello All !! This is Aditya Pandey. I am making this file to learn functionality of version control. Hope you all do the same.Providing some details about git here.')
+
+print (" To test some changes and use git diff command and test it.")
