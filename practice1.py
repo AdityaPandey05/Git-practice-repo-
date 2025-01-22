f69883b88f6ab3b73768b1ec282fb9bf65b41adc
Aditya Pandey <adityapandey0502@gmail.com>
2025-01-22 15:01:20 +0530

    Day2 of learning git

--- practice1.py
+++ practice1.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 print('Hello All !! This is Aditya Pandey. I am making this file to learn functionality of version control. Hope you all do the same.Providing some details about git here.')
 
 print (" To test some changes and use git diff command and test it.")
+=======
+print('Hello All !! This is Aditya Pandey. I am making this file to learn functionality of version control. Hope you all do the same.Providing some details about git here.')
+>>>>>>> d32e856 (Initial commit)
