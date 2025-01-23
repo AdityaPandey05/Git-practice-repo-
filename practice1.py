c0e9e46da240ed802a92fe59251c177d3d6c6b42
Aditya Pandey <adityapandey0502@gmail.com>
2025-01-23 12:30:59 +0530

    day 2 of learning git

--- practice1.py
+++ practice1.py
@@ -1,7 +1,17 @@
-<<<<<<< HEAD
-print('Hello All !! This is Aditya Pandey. I am making this file to learn functionality of version control. Hope you all do the same.Providing some details about git here.')
+# print('Hello All !! This is Aditya Pandey. I am making this file to learn functionality of version control. Hope you all do the same.Providing some details about git here.')
 
-print (" To test some changes and use git diff command and test it.")
-=======
-print('Hello All !! This is Aditya Pandey. I am making this file to learn functionality of version control. Hope you all do the same.Providing some details about git here.')
->>>>>>> d32e856 (Initial commit)
+# print (" To test some changes and use git diff command and test it.")
+# print('Hello All !! This is Aditya Pandey. I am making this file to learn functionality of version control. Hope you all do the same.Providing some details about git here.')
+
+# Day 2 of learning git..
+
+class Solution:
+  def adding(self,a,b):
+    y=a+b
+    print("The final output of adding is :" +str(y))
+  
+obj1=Solution()
+a=int(input("enter the variable a:"))
+b=int(input("enter the variable b:"))
+obj1.adding(a,b)  
+# That was for today. Today I used git checkout main , git stash, git stash apply,git cherry-pick d32e856 . Because yesterday I deatched the head using git checkout thus issue arrised  
