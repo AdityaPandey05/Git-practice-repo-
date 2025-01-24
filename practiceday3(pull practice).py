9d43966d3dd4c6ec4e409c969a8ffd1f6dfc48fd
Aditya Pandey <adityapandey0502@gmail.com>
2025-01-24 12:10:30 +0530

    day 3 practice done

---
+++ practiceday3(pull practice).py
@@ -0,0 +1,9 @@
+#Day 3 of practicing git
+# A short program of python
+# Write a program to print numbers from 1 - 10 with each number printed in a separate line.
+# Solution:----
+for i in range(1,11):
+	print (i)
+# Using (git pull origin) in command prompt I pulled the file created on github repo to local folder. We don't need git merge as it directly shows to the main folder on local desktop folder. But................
+
+# If we create a file and (git fetch) it through the github repo... we have to use ( git merge )as it won't show up in the local working desktop folder.   
