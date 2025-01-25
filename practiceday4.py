bd544544a97442b1932b392d58d6eec5ee48222d
Aditya Pandey <adityapandey0502@gmail.com>
2025-01-25 20:46:32 +0530

    Day 4 of git practice

---
+++ practiceday4.py
@@ -0,0 +1,11 @@
+# day 4 for practice just a push practice 
+
+class Solution:
+  def subtraction(self,a,b):
+    y=a-b
+    print("Required sub is:"+str(y))
+    
+obj2= Solution()
+a= int(input("enter the a:")) 
+b= int(input("enter the b:")) 
+obj2.subtraction(a,b)      
