1afa963bd54cf531ddca63f01203a98fe05a7897
Aditya Pandey <adityapandey0502@gmail.com>
2025-01-27 12:12:24 +0530

    learning branching

---
+++ practiceday5.py
@@ -0,0 +1,11 @@
+# Day5 practice git branching 
+# using git branch (branch name) created a branch
+# using git checkout (branch name) switched to branch 
+# Now this would be uploaded on new branch and would be merged in main branch later 
+
+class Car:
+  color="Blue"
+  Brand="Mercedes"
+car1=Car()
+print(car1.color) 
+print(car1.Brand)   
