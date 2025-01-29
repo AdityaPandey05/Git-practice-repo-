388c8111a9c6ff14bb201b30c0ba420f37d92d14
Aditya Pandey <adityapandey0502@gmail.com>
2025-01-29 12:07:16 +0530

    commit to Day5 branch day7

---
+++ practiceday7.py
@@ -0,0 +1,19 @@
+# Day 7 practice of git
+#Today we will push in next branch itself and merge into main day after tommorow.
+
+class Car:
+  def __init__(self):
+    self.acc=False
+    self.brk=False
+    self.clutch=False
+  def start(self):
+    self.clutch=True
+    self.acc=True
+    print("Car started....")
+
+car1=Car()
+car1.start()    
+# Abstraction is the hiding of implementation details of a class and only showing the essential features to the user.
+
+#Now pushing in the branch Day5 and see tommorow.
+     
